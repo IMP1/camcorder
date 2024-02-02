c4b882ba50670b3ae631ed89b427ae9dd9f6ba68
IMP1 <huw_taylor@hotmail.co.uk>
2024-02-02 20:52:10 +0000

    Fix upto loops using integers

--- src/frame.rb
+++ src/frame.rb
@@ -4,10 +4,11 @@ class Frame
     attr_reader :height
     attr_reader :data
 
-    def initialize(width, height)
+    def initialize(width, height, colour=Colour::BLACK)
         @width = width
         @height = height
-        @data = [0] * @height * @width # Array of 32 bit integers in RGBA format (8 bits per channel)
+        c = Colour.to_rgba(colour)
+        @data = [c] * @height * @width # Array of 32 bit integers in RGBA format (8 bits per channel)
     end
 
     def self.load_from_file(filepath)
@@ -58,9 +59,9 @@ class Frame
         return x >= 0 && x < width
     end
 
-    #
+    ##############################
     # Drawing Methods
-    #
+    ##############################
 
     def fill(colour)
         c = Colour.to_rgba(colour)
@@ -69,10 +70,10 @@ class Frame
 
     def fill_rect(rect, colour)
         c = Colour.to_rgba(colour)
-        x_min = [rect.x, 0].max
-        x_max = [rect.x + rect.width, width].min
+        x_min = [rect.x, 0].max.to_i
+        x_max = [rect.x + rect.width, width].min.to_i
         size = x_max - x_min
-        rect.y.upto(rect.y + rect.height) do |j|
+        rect.y.to_i.upto(rect.y.to_i + rect.height.to_i - 1) do |j|
             next unless include_y?(j)
             n_from = j * width + x_min
             n_to = j * width + x_max
@@ -81,15 +82,49 @@ class Frame
     end
 
     # Copies src_rect from src image to this image at coordinates dst, clipped accordingly to both image bounds.
-    def copy_rect(source_data, source_rect, target_position)
-
+    def copy_rect(source_frame, source_rect, target_position)
+        target_position.y.to_i.upto(target_position.y.to_i + source_rect.height.to_i - 1) do |j|
+            next unless include_y?(j)
+            source_j = j - target_position.y + source_rect.y
+            next unless source_frame.include_y?(source_j)
+            target_position.x.to_i.upto(target_position.x.to_i + source_rect.width.to_i - 1) do |i|
+                next unless include_x?(i)
+                source_i = i - target_position.x + source_rect.x
+                next unless source_frame.include_x?(source_i)
+                @data[j * width + i] = source_frame.data[source_j * source_frame.width + source_i]
+            end
+        end
     end
 
     # Alpha-blends src_rect from src image to this image at coordinates dst, clipped accordingly to both image bounds.
-    def blend_rect(source_data, source_rect, target_position)
-
+    def blend_rect(source_frame, source_rect, target_position)
+        target_position.y.to_i.upto(target_position.y.to_i + source_rect.height.to_i - 1) do |j|
+            next unless include_y?(j)
+            source_j = j - target_position.y + source_rect.y
+            next unless source_frame.include_y?(source_j)
+            target_position.x.to_i.upto(target_position.x.to_i + source_rect.width.to_i - 1) do |i|
+                next unless include_x?(i)
+                source_i = i - target_position.x + source_rect.x
+                next unless source_frame.include_x?(source_i)
+                source_colour = Colour.from_rgba(source_frame.data[source_j * source_frame.width + source_i])
+                target_colour = Colour.from_rgba(@data[j * width + i])
+                @data[j * width + i] = Colour.to_rgba(source_colour.blend(target_colour))
+            end
+        end
     end
 
-    # TODO: Look up the Godot Image class (and the one in LOVE too) for methods to incorporate
+    def map_pixels(rect=nil)
+        rect ||= Rect.new(0, 0, width, height)
+        x_min = [rect.x, 0].max.to_i
+        x_max = [rect.x + rect.width, width].min.to_i
+        size = x_max - x_min
+        rect.y.to_i.upto(rect.y.to_i + rect.height.to_i - 1) do |j|
+            x_min.upto(x_max - 1) do |i|
+                colour = Colour.from_rgba(@data[j * width + i])
+                colour = yield(i, j, colour)
+                @data[j * width + i] = Colour.to_rgba(colour)
+            end
+        end
+    end
 
 end
